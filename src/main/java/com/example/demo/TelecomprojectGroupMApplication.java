41b9c9fc220275ac46d56f9d6baea0f3d02f6aa3
mayuri <mayusable77@gmail.com>
2023-10-09 16:35:32 +0530

    added packages

---
+++ src/main/java/com/example/demo/TelecomprojectGroupMApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelecomprojectGroupMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelecomprojectGroupMApplication.class, args);
+	}
+
+}
